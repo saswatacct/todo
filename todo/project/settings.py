13a6cb8c9835401feac5f21f54cb700563e1328a
Denys Mitlenko <mitlenkodenis3@gmail.com>
2024-10-31 22:13:23 +0200

    feat: add Django crispy forms and Bootstrap5 support to project

--- todo/project/settings.py
+++ todo/project/settings.py
@@ -29,6 +29,8 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "crispy_forms",
+    "crispy_bootstrap5",
 ]
 
 MIDDLEWARE = [
@@ -117,3 +119,7 @@ STATICFILED_DIRS = [BASE_DIR / "static"]
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5"
+
+CRISPY_TEMPLATE_PACK = "bootstrap5"
