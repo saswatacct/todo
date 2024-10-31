0af2e85f17728b9cf9c84eba201fcee28f1439c7
Denys Mitlenko <mitlenkodenis3@gmail.com>
2024-10-31 21:59:35 +0200

    feat: update Django settings to include template and static file directories

--- todo/project/settings.py
+++ todo/project/settings.py
@@ -46,7 +46,9 @@ ROOT_URLCONF = 'todo.project.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            BASE_DIR / 'templates',
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -108,6 +110,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = 'static/'
+
+STATICFILED_DIRS = [BASE_DIR / 'static']
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
