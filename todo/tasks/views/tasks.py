262587e227958898eef1acbc59aa27585f12b5f4
Denys Mitlenko <mitlenkodenis3@gmail.com>
2024-11-01 14:11:12 +0200

    refactor(tasks): simplify priority update logic in PriorityUpdateView

--- todo/tasks/views/tasks.py
+++ todo/tasks/views/tasks.py
@@ -85,13 +85,10 @@ class PriorityUpdateView(LoginRequiredMixin, View):
     def post(self, request: HttpRequest, pk: int, priority: int) -> HttpResponse:
         task: Task = get_object_or_404(Task, pk=pk, project__user=request.user)
 
-        # Ensure the priority is within the bounds of the project's tasks
-        if priority < 1:
-            task.set_priority(1)
-        elif priority > task.project.tasks.count():
-            task.set_priority(task.project.tasks.count())
-        # If the priority is within the bounds, update it if it has changed
-        elif task.priority != priority:
+        # Ensure the priority is greater than 0
+        # and is different from the current task priority.
+        if priority > 0 and priority != task.priority:
+            # Update the task priority
             task.set_priority(priority)
 
         return HttpResponse(status=204)
