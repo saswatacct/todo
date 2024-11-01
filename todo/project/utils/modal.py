9a20f41bb0c24eab8aa647db3dea3ce974025b11
Denys Mitlenko <mitlenkodenis3@gmail.com>
2024-11-01 12:35:31 +0200

    feat(modal): implement modal functionality with show/hide events and templates

---
+++ todo/project/utils/modal.py
@@ -0,0 +1,67 @@
+from collections.abc import Mapping
+from typing import Any
+
+from django.http import HttpRequest, HttpResponse
+from django_htmx.http import trigger_client_event
+
+SHOW_MODAL_EVENT = "show-modal"
+HIDE_MODAL_EVENT = "hide-modal"
+
+
+def show_modal(response: HttpResponse, options: Mapping[str, Any]) -> HttpResponse:
+    """Show a modal after a request.
+
+    Args:
+        response (HttpResponse): The response object.
+        options (Mapping[str, Any]): The options to show the modal.
+
+    Returns:
+        HttpResponse: The response object.
+    """
+
+    return trigger_client_event(response, SHOW_MODAL_EVENT, options)
+
+
+def hide_modal(response: HttpResponse, options: Mapping[str, Any]) -> HttpResponse:
+    """Hide a modal after a request.
+
+    Args:
+        response (HttpResponse): The response object.
+        options (Mapping[str, Any]): The options to show the modal.
+
+    Returns:
+        HttpResponse: The response object.
+    """
+
+    return trigger_client_event(response, HIDE_MODAL_EVENT, options)
+
+
+class ModalMixin:
+    """A mixin to show a modal after a successful form submission."""
+
+    modal_options: Mapping[str, Any] = {}
+
+    def get_modal_options(self) -> Mapping[str, Any]:
+        """Get the options to show the modal.
+
+        Returns:
+            Mapping[str, Any]: The options to show the modal.
+        """
+
+        return self.modal_options
+
+    def dispatch(self, request: HttpRequest, *args, **kwargs) -> HttpResponse:
+        response: HttpResponse = super().dispatch(request, *args, **kwargs)
+
+        if request.htmx:
+            response = show_modal(response, self.get_modal_options())
+
+        return response
+
+    def form_valid(self, *args, **kwargs) -> HttpResponse:
+        response: HttpResponse = super().form_valid(*args, **kwargs)
+
+        if self.request.htmx:
+            response = hide_modal(response, self.get_modal_options())
+
+        return response
---
+++ todo/static/js/modal.js
@@ -0,0 +1,17 @@
+function modalComponent() {
+  return {
+    showModal(event) {
+      // Wait for the next tick to ensure the modal is in the DOM
+      this.$nextTick(() => {
+        const options = event.detail;
+        // Create a new modal instance
+        this.modal = new bootstrap.Modal(this.$el, options);
+        // Show the modal
+        this.modal.show();
+      });
+    },
+    closeModal() {
+      this.modal.hide();
+    },
+  };
+}
